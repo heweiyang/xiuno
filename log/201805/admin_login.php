2c2ef634e7be6428d4eacd290b208c9c1353bbcc
heweiyang <123403213@qq.com>
2018-05-15 17:39:49 +0800

    add xiuno

---
+++ log/201805/admin_login.php
@@ -0,0 +1,1 @@
+<?php exit;?>	2018-05-15 14:59:33	127.0.0.1	/admin/?index-login.htm	1	login successed. uid:1
---
+++ log/201805/php_error.php
@@ -0,0 +1,2 @@
+<?php exit;?>	2018-05-15 15:42:36	127.0.0.1	/admin/?thread-list.htm	1	Error[2]: Invalid argument supplied for foreach(), File: D:\wamp\www\xiunobbs\tmp\admin_route_thread.php, Line: 29
+<?php exit;?>	2018-05-15 15:42:36	127.0.0.1	/admin/?thread-list.htm	1	Error[2]: Invalid argument supplied for foreach(), File: D:\wamp\www\xiunobbs\tmp\admin_view_htm_thread_list.htm, Line: 24
